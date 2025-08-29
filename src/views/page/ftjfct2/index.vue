88a724652d5bbaae81c4dd28fbbde562fd52dc54
辛肃睿 <10704714+xin-surui@user.noreply.gitee.com>
2025-08-29 15:03:16 +0800

    繁体减肥长图2更新

--- src/views/page/ftjfct2/index.vue
+++ src/views/page/ftjfct2/index.vue
@@ -2,7 +2,7 @@
     <div class="ab-container">
         <!-- A页：审核人员及非TikTok用户可见（完全合规内容） -->
         <div v-if="showPageA" class="page-a" style="padding-bottom: 60px;">
-            <img src="@image/ftjfcp-header.png" class="a-image" style="width: 100%;height: 100%;" />
+            <img src="@image/ftjfct2A.jpg" class="a-image" style="width: 100%;height: 100%;" />
             <div class="bottom" style="display: flex;justify-content: space-around;">
                 <LineAddButton :lineList="lineList" />
             </div>
